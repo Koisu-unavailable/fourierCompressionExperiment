2f33a679d62ded9e8da6b5bdace1490ae279fd3a
Koisu <koisuspotify@gmail.com>
2025-07-19 06:25:31 -0700

    Realise

--- fourierCompressionExperiment.py
+++ fourierCompressionExperiment.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.8.22"
+__generated_with = "0.14.11"
 app = marimo.App()
 
 
@@ -8,19 +8,19 @@ app = marimo.App()
 def _(mo):
     mo.md(
         r"""
-        # Main idea
-        An audio file format that stores audio using fourier transforms to decompose the audio into its various signals and then storing the properties of the waves.
+    # Main idea
+    An audio file format that stores audio using fourier transforms to decompose the audio into its various signals and then storing the properties of the waves.
 
-        ## Steps to convert (from a wav file):
-        - Splits the audio into chunks (to make calculations faster)\
-        - Calculates the the fourier transform of each chunks
-        - Get each freqency from each wave from the chunk
-        - Write it to a file
+    ## Steps to convert (from a wav file):
+    - Splits the audio into chunks (to make calculations faster)\
+    - Calculates the the fourier transform of each chunks
+    - Get each freqency from each wave from the chunk
+    - Write it to a file
 
-        ## To reconstruct
-        - Sum up the waves given their frequencies
-        - Do an inverse fourier transform
-        """
+    ## To reconstruct
+    - Sum up the waves given their frequencies
+    - Do an inverse fourier transform
+    """
     )
     return
 
@@ -34,13 +34,37 @@ def _(mo):
 @app.cell
 def _(wavfile):
     SAMPLE_RATE, data = wavfile.read('./input/BadApple.wav')
-    data = data[:, 1]
-    chunk_size = len(data) // 10
+    data = data[:, 1] # the first channel
+    return SAMPLE_RATE, data
+
+
+@app.cell
+def _(data, mo):
+    amount_of_chunks = mo.ui.slider(1, len(data)//100, label="Amount of chunks", value=len(data)//100)
+    amount_of_chunks
+    return (amount_of_chunks,)
+
+
+@app.cell
+def _(amount_of_chunks, data):
+    chunk_size = len(data) // amount_of_chunks.value
     chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
-    data = chunks[0]
-    DURATION = len(data) / SAMPLE_RATE
-    SAMPLES = len(data)
-    return DURATION, SAMPLES, SAMPLE_RATE, chunk_size, chunks, data
+    return (chunks,)
+
+
+@app.cell
+def _(chunks, mo):
+    which_chunk_slider = mo.ui.slider(1, len(chunks), label="Which chunk to choose")
+    which_chunk_slider
+    return (which_chunk_slider,)
+
+
+@app.cell
+def _(SAMPLE_RATE, chunks, which_chunk_slider):
+    chunk = chunks[which_chunk_slider.value]
+    DURATION = len(chunk) / SAMPLE_RATE
+    SAMPLES = len(chunk)
+    return DURATION, chunk
 
 
 @app.cell
@@ -50,10 +74,19 @@ def _(mo):
 
 
 @app.cell
-def _(DURATION, SAMPLE_RATE, data, np):
+def _(DURATION, SAMPLE_RATE, chunk, np, plt):
     times = np.linspace(0, DURATION, int(DURATION*SAMPLE_RATE))
-    signal_values = [data[int(time * SAMPLE_RATE) - 1] for time in times]
+    signal_values = [chunk[int(time * SAMPLE_RATE) - 1] for time in times]
+    plt.plot(times, signal_values)
+    plt.title("The audio chunk represented of a signal of the seconds domain")
+    plt.show()
     return signal_values, times
+
+
+@app.cell
+def _(mo, signal_values):
+    mo.md(f"""There are {len(signal_values)} samples""")
+    return
 
 
 @app.cell
@@ -63,58 +96,57 @@ def _(mo):
 
 
 @app.cell
-def _(data, np, plt, signal_values, times):
-    plt.plot(times, signal_values)
-    plt.title("The audio chunks represented of a signal of the seconds domain")
-    plt.show()
-    fft = np.fft.fft(data)
-    fft = fft[:len(fft) // 2]
-    fft = abs(fft)
-    return (fft,)
-
-
-@app.cell
 def _(mo):
     mo.md(
         """
-        Compute the actual freqeuncies. \n
-        1. We use `np.fftfreq` the get the frequencies that could appear in the fourier transform (i think) 
-        2. We find the peaks. The peaks correspond to the most prevalent frequencies 
-        3. We graph the fft along with the peaks
-        4. We append the frequency at `peak` to the `result` array
-        5. We reconstruct the signal using the `result`
-        """
+    Compute the actual freqeuncies. \n
+    1. We use `np.fftfreq` the get the frequencies that could appear in the fourier transform (i think) 
+    2. We find the peaks. The peaks correspond to the most prevalent frequencies 
+    3. We graph the fft along with the peaks
+    4. We append the frequency at `peak` to the `result` array
+    5. We reconstruct the signal using the `result`
+    """
     )
     return
 
 
 @app.cell
-def _(SAMPLE_RATE, data, fft, find_peaks, np, plt):
-    frequencies = np.fft.fftfreq(data.size, d=1 / SAMPLE_RATE)[:data.size // 2]
-    print(len(frequencies))
+def _(SAMPLE_RATE, chunk, find_peaks, np, plt):
+
+    fft = np.fft.fft(chunk)
+    fft = fft[:len(fft) // 2]
+    frequencies = np.fft.fftfreq(chunk.size, d=1 / SAMPLE_RATE)[:chunk.size // 2]
+    plt.title("The fourier transform of the signal")
+    plt.plot(frequencies, fft)
+    plt.show()
     peaks, props = find_peaks(fft)
-    # plt.plot(np.linspace(0, len(fft), len(fft)), fft)
-    # plt.grid(True)
+    return fft, frequencies, peaks
+
+
+@app.cell
+def _(fft, frequencies, njit, np, peaks, plt):
+
     freqs = []
     for peak in peaks:
         plt.text(peak, fft[peak], f'{peak:.2f}', ha='center', fontsize=8)
         freqs.append(frequencies[peak])
-    print(f'Freqs: {freqs}')
+    plt.title("The fourier transform of the signal (with freqs)")
+    plt.plot(frequencies, fft)
+    plt.show()
     times_ran = 0
-    from numba import njit
     @njit
-    def func(x):
+    def reconstructed_wave(x):
         ans  = 0
 
         for peak in peaks:
             ans += np.sin(x * 2 * np.pi * frequencies[peak])
         return ans
     # plt.show()
-    # plt.plot(times, [func(x) for x in times])
+    # plt.plot(times, [reconstructed_wave(x) for x in times])
     # print(times_ran)
     # plt.title("Reconstructed wave")
     # plt.show()
-    return freqs, frequencies, func, njit, peak, peaks, props, times_ran
+    return (reconstructed_wave,)
 
 
 @app.cell
@@ -124,38 +156,165 @@ def _(mo):
 
 
 @app.cell
-def _(DURATION, SAMPLE_RATE, func, mo, njit, np, plt, times, wavfile):
-    amount_slider = mo.ui.slider(1, 1000, label="$\\omega$", show_value=True)
-    amount_slider
-    _times = np.linspace(0, DURATION, int(DURATION * SAMPLE_RATE))
-    @njit(parallel=True)
-    def test(times):
+def _(
+    SAMPLE_RATE,
+    njit,
+    np,
+    plt,
+    reconstructed_wave,
+    signal_values,
+    times,
+    wavfile,
+):
+
+    # TODO: MAKE A KERNEL FOR THIS
+    @njit()
+    def reconstruct(times):
         result = []
         for time in times:
-            result.append(func(time))
+            result.append(reconstructed_wave(time))
         return result
-    good = test(times)
-    wavfile.write("test.wav", SAMPLE_RATE,  np.array(good))
-
-    plt.plot(times, good)
-    return amount_slider, good, test
-
-
-@app.cell
-def _(SAMPLE_RATE, data, good, wavfile):
-    wavfile.write("test2.wav", SAMPLE_RATE, data)
-    print(len(good))
-    print(len(data))
+    new_wave = reconstruct(times)
+    wavfile.write("reconstructed_wave.wav", SAMPLE_RATE,  np.array(new_wave))
+    plt.figure(figsize=(20,6))
+    plt.title("The reconstructed wave")
+    plt.plot(times, new_wave, color="blue", label="Reconstucted wave")
+    plt.plot(times, signal_values, color="red", label="Original wave")
+    plt.legend()
     return
 
 
 @app.cell
-def _(amount_slider, fft, np):
-    freqs2 = []
-    amount = amount_slider.value
-    distance_between_points = np.floor(len(fft) / amount)
-    print(f"Sampling ~{amount} points ~{distance_between_points} apart")
-    return amount, distance_between_points, freqs2
+def _(mo):
+    mo.md("""As you can see, we have a small problem. We didn't account for the __*strength*__ of the frequencies, we assumed their all equal which is almost never the case. Let's fix that.""")
+    return
+
+
+@app.cell
+def _(fft, frequencies, np, peaks, plt):
+
+    better_result_freqs : dict[float, float] = {} 
+    for _freq in frequencies:
+        index = np.where(frequencies == _freq)
+
+        better_result_freqs[fft[index[0]][0]] = _freq
+    print(peaks)
+    print(better_result_freqs)
+    plt.title("Peaks and strengths ")
+    plt.xlabel("Freqs")
+    plt.ylabel("Strengths")
+    plt.plot(better_result_freqs.values(), better_result_freqs.keys())
+    plt.show()
+    max_value = max(better_result_freqs.keys())
+    better_result_freqs = {k / max_value : v for k, v in better_result_freqs.items()}
+    print(better_result_freqs)
+    def better_reconstructed_wave(x):
+        ans  = 0
+        for strength, freq in better_result_freqs.items():
+        
+            ans += np.real(strength) * np.sin(x * 2 * np.pi * freq + np.imag(strength))
+        return ans
+    return (better_reconstructed_wave,)
+
+
+@app.cell
+def _(better_reconstructed_wave, np, plt, signal_values, times):
+
+    # TODO: MAKE A KERNEL FOR THIS
+
+    def better_reconstruct(times):
+        _result = []
+        for _time in times:
+            _result.append(better_reconstructed_wave(_time))
+        return _result
+    _new_wave = better_reconstruct(times)
+    # wavfile.write("reconstructed_wave.wav", SAMPLE_RATE,  np.array(_new_wave))
+    # wavfile.write("old bad wave.wav", SAMPLE_RATE,  np.array(signal_values))
+    normalised_signal_values = normalise(np.array(signal_values))
+    _new_wave = normalise(np.array(_new_wave))
+    plt.figure(figsize=(20,6))
+    plt.title("The reconstructed wave")
+    plt.plot(times, _new_wave, color="blue", label="Reconstucted wave")
+    plt.plot(times, normalised_signal_values, color="red", label="Original wave")
+    plt.legend()
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md("Now we do that for all the chunks")
+    return
+
+
+@app.cell
+def _():
+    final_wave = []
+    return (final_wave,)
+
+
+@app.cell
+def _():
+
+    
+    return
+
+
+app._unparsable_cell(
+    r"""
+    d = len(data) / SAMPLE_RATE
+    s = SAMPLE_RATE * d
+
+    ts = np.linspace(0, d, int(d*SAMPLE_RATE))
+
+    print(type(final_wave[0]))
+
+    final_wave2 = []
+    for arr in final_wave:
+        if type(arr) == list:
+            for val in arr:  # ✅ just iterate over arr directly
+                final_wave2.append(val)
+        else:
+            final_wave2.append(arr)
+    total_duration = len(final_wave2) / SAMPLE_RATE
+    good_i = 0
+    for i in final_wave2:
+    
+        if i != 0:
+            good_i = final_wave2.index(i)
+            break;
+    print(len(final_wave2[good_i:]))
+    ts = np.linspace(0, total_duration, len(final_wave2))
+    plt.plot(times, , label=\"FInal\")
+    plt.show()
+    wavfile.write(\"test.wav\", SAMPLE_RATE, np.array(final_wave2))
+    """,
+    name="_"
+)
+
+
+@app.cell
+def _(chunks, mo):
+    sy = mo.ui.slider(1, len(chunks), label="Which chunk to choose2")
+    sy
+    return (sy,)
+
+
+@app.cell
+def _(SAMPLE_RATE, final_wave, np, plt, sy):
+
+    good_chunk = final_wave[sy.value]
+
+    tsy = np.linspace(0, len(good_chunk) / SAMPLE_RATE, len(good_chunk))
+    plt.plot(tsy, good_chunk)
+    plt.title(sy.value)
+    return
+
+
+@app.function
+# HELPERS:
+def normalise(arr):
+    max_value = max(arr)
+    return arr/max_value
 
 
 @app.cell
@@ -166,18 +325,8 @@ def _():
     from scipy.signal import find_peaks, butter, find_peaks_cwt
     import numpy as np
     import matplotlib.pyplot as plt
-    import cupy as cp
-    return (
-        butter,
-        cp,
-        find_peaks,
-        find_peaks_cwt,
-        marimo,
-        mo,
-        np,
-        plt,
-        wavfile,
-    )
+    from numba import njit
+    return find_peaks, mo, njit, np, plt, wavfile
 
 
 if __name__ == "__main__":
