4ef5662361ecb4d1e7e029dbb46531015c6a26fe
Koisu <koisuspotify@gmail.com>
2025-11-25 19:58:34 -0800

    doesn't wor :sob:

--- fourierCompressionExperiment.py
+++ fourierCompressionExperiment.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.14.11"
+__generated_with = "0.14.12"
 app = marimo.App()
 
 
@@ -27,7 +27,7 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    mo.md("""We read the file and get a small chunk to show. We also get the metadata about the chunk""")
+    mo.md(r"""We read the file and get a small chunk to show. We also get the metadata about the chunk""")
     return
 
 
@@ -69,7 +69,7 @@ def _(SAMPLE_RATE, chunks, which_chunk_slider):
 
 @app.cell
 def _(mo):
-    mo.md("""Get the graph of the signal as of function of time in seconds""")
+    mo.md(r"""Get the graph of the signal as of function of time in seconds""")
     return
 
 
@@ -91,14 +91,14 @@ def _(mo, signal_values):
 
 @app.cell
 def _(mo):
-    mo.md("""Compute the fourier transform""")
+    mo.md(r"""Compute the fourier transform""")
     return
 
 
 @app.cell
 def _(mo):
     mo.md(
-        """
+        r"""
     Compute the actual freqeuncies. \n
     1. We use `np.fftfreq` the get the frequencies that could appear in the fourier transform (i think) 
     2. We find the peaks. The peaks correspond to the most prevalent frequencies 
@@ -111,202 +111,48 @@ def _(mo):
 
 
 @app.cell
-def _(SAMPLE_RATE, chunk, find_peaks, np, plt):
-
+def _(SAMPLE_RATE, chunk, np, plt):
     fft = np.fft.fft(chunk)
+    full_fft = fft
     fft = fft[:len(fft) // 2]
     frequencies = np.fft.fftfreq(chunk.size, d=1 / SAMPLE_RATE)[:chunk.size // 2]
     plt.title("The fourier transform of the signal")
     plt.plot(frequencies, fft)
     plt.show()
-    peaks, props = find_peaks(fft)
-    return fft, frequencies, peaks
+
+    return (full_fft,)
 
 
 @app.cell
-def _(fft, frequencies, njit, np, peaks, plt):
-
-    freqs = []
-    for peak in peaks:
-        plt.text(peak, fft[peak], f'{peak:.2f}', ha='center', fontsize=8)
-        freqs.append(frequencies[peak])
-    plt.title("The fourier transform of the signal (with freqs)")
-    plt.plot(frequencies, fft)
-    plt.show()
-    times_ran = 0
-    @njit
-    def reconstructed_wave(x):
-        ans  = 0
-
-        for peak in peaks:
-            ans += np.sin(x * 2 * np.pi * frequencies[peak])
-        return ans
-    # plt.show()
-    # plt.plot(times, [reconstructed_wave(x) for x in times])
-    # print(times_ran)
-    # plt.title("Reconstructed wave")
-    # plt.show()
-    return (reconstructed_wave,)
-
-
-@app.cell
-def _(mo):
-    mo.md("""As you can see, the wave isn't very accurate, let's try a different method. We'll sample $\\omega$ amount of equidistant points from the graph""")
-    return
-
-
-@app.cell
-def _(
-    SAMPLE_RATE,
-    njit,
-    np,
-    plt,
-    reconstructed_wave,
-    signal_values,
-    times,
-    wavfile,
-):
-
-    # TODO: MAKE A KERNEL FOR THIS
-    @njit()
-    def reconstruct(times):
-        result = []
-        for time in times:
-            result.append(reconstructed_wave(time))
-        return result
-    new_wave = reconstruct(times)
-    wavfile.write("reconstructed_wave.wav", SAMPLE_RATE,  np.array(new_wave))
-    plt.figure(figsize=(20,6))
-    plt.title("The reconstructed wave")
-    plt.plot(times, new_wave, color="blue", label="Reconstucted wave")
-    plt.plot(times, signal_values, color="red", label="Original wave")
+def _(chunk, full_fft, np, plt, times):
+    plt.title("FFT vs. Normal")
+    plt.plot(times, chunk, label="Orginal")
+    plt.plot(times, np.fft.ifft(full_fft), label="Fourier transformed, then inverse Fourier Transformed")
     plt.legend()
     return
 
 
 @app.cell
 def _(mo):
-    mo.md("""As you can see, we have a small problem. We didn't account for the __*strength*__ of the frequencies, we assumed their all equal which is almost never the case. Let's fix that.""")
-    return
-
-
-@app.cell
-def _(fft, frequencies, np, peaks, plt):
-
-    better_result_freqs : dict[float, float] = {} 
-    for _freq in frequencies:
-        index = np.where(frequencies == _freq)
-
-        better_result_freqs[fft[index[0]][0]] = _freq
-    print(peaks)
-    print(better_result_freqs)
-    plt.title("Peaks and strengths ")
-    plt.xlabel("Freqs")
-    plt.ylabel("Strengths")
-    plt.plot(better_result_freqs.values(), better_result_freqs.keys())
-    plt.show()
-    max_value = max(better_result_freqs.keys())
-    better_result_freqs = {k / max_value : v for k, v in better_result_freqs.items()}
-    print(better_result_freqs)
-    def better_reconstructed_wave(x):
-        ans  = 0
-        for strength, freq in better_result_freqs.items():
-        
-            ans += np.real(strength) * np.sin(x * 2 * np.pi * freq + np.imag(strength))
-        return ans
-    return (better_reconstructed_wave,)
-
-
-@app.cell
-def _(better_reconstructed_wave, np, plt, signal_values, times):
-
-    # TODO: MAKE A KERNEL FOR THIS
-
-    def better_reconstruct(times):
-        _result = []
-        for _time in times:
-            _result.append(better_reconstructed_wave(_time))
-        return _result
-    _new_wave = better_reconstruct(times)
-    # wavfile.write("reconstructed_wave.wav", SAMPLE_RATE,  np.array(_new_wave))
-    # wavfile.write("old bad wave.wav", SAMPLE_RATE,  np.array(signal_values))
-    normalised_signal_values = normalise(np.array(signal_values))
-    _new_wave = normalise(np.array(_new_wave))
-    plt.figure(figsize=(20,6))
-    plt.title("The reconstructed wave")
-    plt.plot(times, _new_wave, color="blue", label="Reconstucted wave")
-    plt.plot(times, normalised_signal_values, color="red", label="Original wave")
-    plt.legend()
+    mo.md(r"""As you can see, they're the same! The fourier transform is reversible""")
     return
 
 
 @app.cell
 def _(mo):
-    mo.md("Now we do that for all the chunks")
+    mo.md("We attempt to smooth it")
     return
 
 
 @app.cell
-def _():
-    final_wave = []
-    return (final_wave,)
+def _(chunk, find_peaks, full_fft, np, plt, times):
+    peaks = find_peaks(full_fft)[0]
 
+    smooth_fft  = [value if list(full_fft).index(value) in peaks else 0 for value in full_fft]
 
-@app.cell
-def _():
-
-    
-    return
-
-
-app._unparsable_cell(
-    r"""
-    d = len(data) / SAMPLE_RATE
-    s = SAMPLE_RATE * d
-
-    ts = np.linspace(0, d, int(d*SAMPLE_RATE))
-
-    print(type(final_wave[0]))
-
-    final_wave2 = []
-    for arr in final_wave:
-        if type(arr) == list:
-            for val in arr:  # ✅ just iterate over arr directly
-                final_wave2.append(val)
-        else:
-            final_wave2.append(arr)
-    total_duration = len(final_wave2) / SAMPLE_RATE
-    good_i = 0
-    for i in final_wave2:
-    
-        if i != 0:
-            good_i = final_wave2.index(i)
-            break;
-    print(len(final_wave2[good_i:]))
-    ts = np.linspace(0, total_duration, len(final_wave2))
-    plt.plot(times, , label=\"FInal\")
-    plt.show()
-    wavfile.write(\"test.wav\", SAMPLE_RATE, np.array(final_wave2))
-    """,
-    name="_"
-)
-
-
-@app.cell
-def _(chunks, mo):
-    sy = mo.ui.slider(1, len(chunks), label="Which chunk to choose2")
-    sy
-    return (sy,)
-
-
-@app.cell
-def _(SAMPLE_RATE, final_wave, np, plt, sy):
-
-    good_chunk = final_wave[sy.value]
-
-    tsy = np.linspace(0, len(good_chunk) / SAMPLE_RATE, len(good_chunk))
-    plt.plot(tsy, good_chunk)
-    plt.title(sy.value)
+    plt.plot(times, smooth_fft)
+    plt.plot(times, np.fft.ifft(smooth_fft))
+    plt.plot(times, chunk)
     return
 
 
@@ -326,7 +172,8 @@ def _():
     import numpy as np
     import matplotlib.pyplot as plt
     from numba import njit
-    return find_peaks, mo, njit, np, plt, wavfile
+    import random as rdm
+    return find_peaks, mo, np, plt, wavfile
 
 
 if __name__ == "__main__":
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fourierexperiment!")
+
+
+if __name__ == "__main__":
+    main()
